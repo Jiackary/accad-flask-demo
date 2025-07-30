3e675d06a06af8cbc95cd3f27a8c4500d27aa7a6
Zachary <zacchengy123@gmail.com>
2025-07-30 10:36:31 +0800

    Adds more functionality to basic templates for flask app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,5 @@ def hello_world():
     return "<p>Hello, world</p>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=os.environ.get('PORT', 5000), host='0.0.0.0', debug=True) 
+    #turning host to 0.0.0.0 makes it public so it can be hosted
